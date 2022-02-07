2759b41810f75781710974eb81bd67d471c1ccbc
Candiepih <mutheeal.am@gmail.com>
2022-02-07 11:39:11 +0300

    Initial commit

---
+++ utils/db.js
@@ -0,0 +1,43 @@
+import {
+  MongoClient,
+} from 'mongodb';
+import {
+  env,
+} from 'process';
+
+class DBClient {
+  constructor() {
+    this.host = env.DB_HOST || 'localhost';
+    this.port = env.DB_PORT || 27017;
+    this.dbName = env.DB_DATABASE || 'files_manager';
+    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    this.client.connect().then(() => {
+      this.db = this.client.db(this.dbName);
+    }).catch((err) => {
+      console.error(err.message);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.db.collection('users');
+    const nb = await collection.countDocuments();
+    return nb;
+  }
+
+  async nbFiles() {
+    const collection = this.db.collection('files');
+    const nb = await collection.countDocuments();
+    return nb;
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
---
+++ utils/redis.js
@@ -0,0 +1,47 @@
+/**
+ * Contains redis client class and some helper functions
+ */
+import {
+  createClient,
+} from 'redis';
+import {
+  promisify,
+} from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log(err.message);
+    });
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const result = await getAsync(key);
+    return result;
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    const result = await setAsync(key, value, 'EX', duration);
+    return result;
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    const result = await delAsync(key);
+    return result;
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
