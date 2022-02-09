8551d0c14c0e42e4c5537c52eaa5979492cc3e44
Candiepih <mutheeal.am@gmail.com>
2022-02-09 12:47:38 +0300

    Get port from environment variable

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {
-  env,
-} from 'process';
 import router from './routes';
 
 const app = express();
-const port = env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
