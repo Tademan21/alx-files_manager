08719d2ff6ff4df3343e3c49994ef29ad454434c
Candiepih <mutheeal.am@gmail.com>
2022-02-09 13:04:18 +0300

    changed router route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './routes';
+import router from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
