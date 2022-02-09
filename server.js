d91e7b2b2393a4561c2ea103526923d94ef8e04d
Candiepih <mutheeal.am@gmail.com>
2022-02-09 14:22:15 +0300

    Removed body parser

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
 app.use(router);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
